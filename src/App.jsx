d1f82a26b379826844229a7d058f36d7d8fb0c06
Mahdi <meiti.edu@gmail.com>
2022-12-18 15:19:46 +0330

    create diffrent states and make pagination dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,80 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./App.css";
 import User from "./user";
 
 function App() {
-  const [data, setData] = useState(true);
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState([]);
+  const [users, setUsers] = useState(null);
+  const [btnNum, setBtnNum] = useState([]);
+  useEffect(() => {
+    axios(" https://api.github.com/users").then((res) => setData(res.data));
+  }, []);
+  useEffect(() => {
+    const start = count * 6;
+    const end = start + 6;
+    console.log(start, end);
+    setUsers(data.slice(start, end));
+  }, [count]);
+  useEffect(() => {
+    setUsers(data.slice(0, 6));
+    const newBtnArr = [];
+    for (let i = 0; i < data.length / 6; i++) {
+      newBtnArr.push(`${i}`);
+    }
+    setBtnNum(newBtnArr);
+  }, [data]);
 
-  if (!data) {
+  const clickHandler = (event) => {
+    setCount(+event.target.innerText - 1);
+  };
+
+  if (!data.length) {
     return <h1 className="loading">Loading ...</h1>;
   }
   return (
     <div className="container">
       <div className="users">
-        <User />
-        <User />
-        <User />
+        {users.map((user) => {
+          return <User {...user} />;
+        })}
       </div>
       <div className="btns">
-        <button>Previuos</button>
-        <button>1</button>
-        <button>1</button>
-        <button>1</button>
-        <button>1</button>
-        <button>Next</button>
+        <button
+          onClick={() => {
+            if (count > 0) {
+              setCount(count - 1);
+            }
+          }}
+        >
+          Previuos
+        </button>
+        {btnNum.map((element, index) => {
+          if (index === count) {
+            return (
+              <button
+                onClick={() => clickHandler(event)}
+                style={{ background: "black" }}
+              >
+                {index + 1}
+              </button>
+            );
+          } else {
+            return (
+              <button onClick={() => clickHandler(event)}>{index + 1}</button>
+            );
+          }
+        })}
+        <button
+          onClick={() => {
+            if (count < data.length / 6 - 1) {
+              setCount(count + 1);
+            }
+          }}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
